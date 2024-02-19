7bf07b611ffbe6ed6aa2177113d5ec1df248c9d8
HenrikVP <hpo@tec.dk>
2024-02-19 10:35:57 +0100

    Initial commit

---
+++ src/main/java/org/example/gamesapiv3/GamesApiV3Application.java
@@ -0,0 +1,13 @@
+package org.example.gamesapiv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesApiV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesApiV3Application.class, args);
+	}
+
+}
